3579c15316819183523547b0672ee142c9a88f9d
Bastien MOUGET <52538883+BastienMOUGET@users.noreply.github.com>
2026-05-13 08:05:40 +0200

    release: v0.7.5 (#362)

--- src/amc_lib.c
+++ src/amc_lib.c
@@ -4749,7 +4749,7 @@ code_string Amalgame_Compiler_PackageRegistry_AmalgameTypeFromC(code_string cTyp
 }
 
 code_string Amalgame_Compiler_PackageRegistry_AmcVersion() {
-    return "0.7.4";
+    return "0.7.5";
 }
 
 i64 Amalgame_Compiler_PackageRegistry_SupportedManifestSchema() {
