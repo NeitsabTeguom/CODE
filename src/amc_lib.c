9887b450927c23e0f259923b20d5a7913205fe4e
Bastien MOUGET <52538883+BastienMOUGET@users.noreply.github.com>
2026-05-14 07:22:40 +0200

    release: v0.8.2 (#403)

--- src/amc_lib.c
+++ src/amc_lib.c
@@ -6258,7 +6258,7 @@ code_string Amalgame_Compiler_PackageRegistry_AmalgameTypeFromC(code_string cTyp
 
 code_string Amalgame_Compiler_PackageRegistry_AmcVersion() {
     #line 577 "./src/package_registry.am"
-    return "0.8.1";
+    return "0.8.2";
 }
 
 i64 Amalgame_Compiler_PackageRegistry_SupportedManifestSchema() {
@@ -21420,12 +21420,12 @@ Amalgame_Compiler_BuildInfo* Amalgame_Compiler_BuildInfo_new() {
 
 code_string Amalgame_Compiler_BuildInfo_GitRev() {
     #line 26 "./src/stdlib/amc_buildinfo.am"
-    return "013605d7";
+    return "8f83cf6f";
 }
 
 code_string Amalgame_Compiler_BuildInfo_BuildDate() {
     #line 30 "./src/stdlib/amc_buildinfo.am"
-    return "2026-05-14T05:00:33Z";
+    return "2026-05-14T05:14:56Z";
 }
 
 Amalgame_Compiler_LspServer* Amalgame_Compiler_LspServer_new();
