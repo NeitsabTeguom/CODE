e7a7885adcddc29fce4fdd2c4e771e7442ba18d4
Bastien Mouget <bm@neitsab.fr>
2026-05-16 10:42:41 +0200

    release: v0.8.16 — new X(...).Method() chain codegen fix

--- src/amc_lib.c
+++ src/amc_lib.c
@@ -6395,7 +6395,7 @@ code_string Amalgame_Compiler_PackageRegistry_AmalgameTypeFromC(code_string cTyp
 
 code_string Amalgame_Compiler_PackageRegistry_AmcVersion() {
     #line 577 "./src/package_registry.am"
-    return "0.8.15";
+    return "0.8.16";
 }
 
 i64 Amalgame_Compiler_PackageRegistry_SupportedManifestSchema() {
@@ -21954,12 +21954,12 @@ Amalgame_Compiler_BuildInfo* Amalgame_Compiler_BuildInfo_new() {
 
 code_string Amalgame_Compiler_BuildInfo_GitRev() {
     #line 26 "./src/stdlib/amc_buildinfo.am"
-    return "0807d926";
+    return "55359235";
 }
 
 code_string Amalgame_Compiler_BuildInfo_BuildDate() {
     #line 30 "./src/stdlib/amc_buildinfo.am"
-    return "2026-05-16T08:35:37Z";
+    return "2026-05-16T08:42:14Z";
 }
 
 Amalgame_Compiler_LspServer* Amalgame_Compiler_LspServer_new();
