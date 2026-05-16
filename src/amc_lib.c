6805d8b46c703a1b7d6aff23dd5acb1988d31db5
Bastien Mouget <bm@neitsab.fr>
2026-05-16 07:39:32 +0200

    release: v0.8.15 — InferTypeFromExpr memoization fix

--- src/amc_lib.c
+++ src/amc_lib.c
@@ -6395,7 +6395,7 @@ code_string Amalgame_Compiler_PackageRegistry_AmalgameTypeFromC(code_string cTyp
 
 code_string Amalgame_Compiler_PackageRegistry_AmcVersion() {
     #line 577 "./src/package_registry.am"
-    return "0.8.14";
+    return "0.8.15";
 }
 
 i64 Amalgame_Compiler_PackageRegistry_SupportedManifestSchema() {
@@ -21882,12 +21882,12 @@ Amalgame_Compiler_BuildInfo* Amalgame_Compiler_BuildInfo_new() {
 
 code_string Amalgame_Compiler_BuildInfo_GitRev() {
     #line 26 "./src/stdlib/amc_buildinfo.am"
-    return "fc32b4ab";
+    return "cb7413e0";
 }
 
 code_string Amalgame_Compiler_BuildInfo_BuildDate() {
     #line 30 "./src/stdlib/amc_buildinfo.am"
-    return "2026-05-16T05:30:01Z";
+    return "2026-05-16T05:36:13Z";
 }
 
 Amalgame_Compiler_LspServer* Amalgame_Compiler_LspServer_new();
