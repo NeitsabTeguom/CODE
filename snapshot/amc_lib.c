02ff6dd285134666beed686e1e9fc6ddc742ef43
Bastien MOUGET <52538883+BastienMOUGET@users.noreply.github.com>
2026-05-16 20:16:10 +0200

    release: v0.8.18 (#486)
    - Bump AmcVersion() to 0.8.18
    - CHANGELOG: [v0.8.18] section documents Process v2 (split stderr,
      timeout-aware variants, sentinel exit 124)
    - CHANGELOG footer: backfill v0.8.15-17 link defs + add v0.8.18
    - README: bump current version to v0.8.18
    - ROADMAP: extend priority sequence + Updated header banner
      (mentions Process v2 / 452/452 tests / amc 0.8.18) ; tick "Process
      v2" item in "How to pick the next thing"
    - snapshot/* refreshed

--- snapshot/amc_lib.c
+++ snapshot/amc_lib.c
@@ -6405,7 +6405,7 @@ code_string Amalgame_Compiler_PackageRegistry_AmalgameTypeFromC(code_string cTyp
 
 code_string Amalgame_Compiler_PackageRegistry_AmcVersion() {
     #line 577 "./src/package_registry.am"
-    return "0.8.17";
+    return "0.8.18";
 }
 
 i64 Amalgame_Compiler_PackageRegistry_SupportedManifestSchema() {
@@ -21982,12 +21982,12 @@ Amalgame_Compiler_BuildInfo* Amalgame_Compiler_BuildInfo_new() {
 
 code_string Amalgame_Compiler_BuildInfo_GitRev() {
     #line 26 "./src/stdlib/amc_buildinfo.am"
-    return "957785d9";
+    return "e8b8cb3b";
 }
 
 code_string Amalgame_Compiler_BuildInfo_BuildDate() {
     #line 30 "./src/stdlib/amc_buildinfo.am"
-    return "2026-05-16T17:11:31Z";
+    return "2026-05-16T18:12:16Z";
 }
 
 Amalgame_Compiler_LspServer* Amalgame_Compiler_LspServer_new();
